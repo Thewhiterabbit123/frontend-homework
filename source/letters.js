6946241f2e6dfd019bf6f6f5feb081fa46779899
Thewhiterabbit123 <nivasilieva13@gmail.com>
2018-02-18 01:47:35 +0300

    some changes

--- source/letters.js
+++ source/letters.js
@@ -1,8 +1,12 @@
 'use strict';
 
-const uniUndef = ( msg, letter, index, letterDelete ) => {
+const uniUndef = ( msg, letter, index, letterDelete, flag ) => {
 
-    if (msg.indexOf( letter, index + 1 ) !== -1) {
+    if ( flag !== undefined ) {
+        return msg.indexOf( letter, index + 1 ) === -1;
+    }
+
+    if ( msg.indexOf( letter, index + 1 ) !== -1 ) {
         letterDelete.add(letter);
     }
     return !letterDelete.has(letter);
@@ -11,16 +15,10 @@ const uniUndef = ( msg, letter, index, letterDelete ) => {
 const letters = ( msg, flag ) => {
     let letterDelete = new Set();
 
-    if( flag === undefined ) {
-         return [...msg].filter( ( letter, index ) => {
-             return uniUndef( msg, letter, index, letterDelete );
-         }).join( '' );
-    }
-
     flag === true ? msg = [...msg].reverse().join('') : msg;
 
     let outStr = [...msg].filter( ( letter, index ) => {
-        return msg.indexOf( letter, index + 1 ) === -1;
+        return uniUndef( msg, letter, index, letterDelete, flag );
     }).join( '' );
 
     return flag === true ? [...outStr].reverse().join( '' ) : outStr;
